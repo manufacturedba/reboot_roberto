9793ae81360818c214b65a403c3a9af6542f495c
manufacturedba <manufacturedba@gmail.com>
2014-09-02 18:24:34 -0500

    Grunt watcher for sass

---
+++ develop/Gruntfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var base_path = '../reboot/';
+var static_path = base_path + 'static/';
+
+// Necessary because Sass will generate a sass/ directory in css/
+var sass_path = static_path + 'sass/';
+
+module.exports = function(grunt){
+    require('load-grunt-tasks')(grunt);
+    
+    grunt.initConfig({
+        watch: {
+          sass: {
+            files: [static_path + 'sass/*.scss'],
+            tasks: ['sass']
+          }
+        },
+        sass: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: sass_path,
+                    src: ['*.scss'],
+                    dest: static_path + 'css',
+                    ext: '.css'
+                }],
+                options: {
+                    style: 'compact' 
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('static', ['watch'])
+}
