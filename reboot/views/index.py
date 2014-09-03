4c132ef899cd8c6353994539848b04898ba4c8dc
manufacturedba <manufacturedba@gmail.com>
2014-09-03 00:44:08 -0500

    Moved index rendering to view. Added entries function for returning JSON of entries

---
+++ reboot/views/index.py
@@ -0,0 +1,32 @@
+"""
+Views for loading pages with context
+"""
+from zinnia.models.entry import Entry
+
+from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+
+import json
+
+import sys
+
+class JsonResponse(HttpResponse):
+    def __init__(self, content={}, mimetype=None, status=None,
+             content_type='application/json'):
+        super(JsonResponse, self).__init__(json.dumps(content), mimetype=mimetype,
+                                           status=status, content_type=content_type)
+                                           
+class HomeView(TemplateView):
+    template_name = "base.html"
+        
+def entries(request):
+    data = []
+    recent_entries = Entry.objects.all()[:5]
+    for entry in recent_entries:
+        post = {'title': entry.title, 'content': entry.content, 'comments': [], 'creation_date': entry.creation_date.isoformat()}
+        for comment in entry.comments:
+            comment_obj = {'comment': comment.comment, 'public': comment.is_public}
+            post['comments'].append(comment)
+        data.append(post)
+    print >> sys.stderr, data
+    return JsonResponse(data)
