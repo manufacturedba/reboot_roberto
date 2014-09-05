9494317238e9d6a4a7eddf628aa0eac568044b6c
manufacturedba <manufacturedba@gmail.com>
2014-09-05 04:42:25 -0500

    Added watcher to entry.html to know when entries are loaded. Hiding entire entry div if nothing is loaded

--- reboot/static/js/controllers/entryCtrl.js
+++ reboot/static/js/controllers/entryCtrl.js
@@ -2,13 +2,27 @@
 
 var mod = angular.module('rebootRoberto.controllers.entry', ['rebootRoberto.services.blog']);
 
-mod.controller('entryCtrl', ['$scope', '$routeParams', 'blogSrvc', function(scope, routeParams, blog){
+mod.controller('entryCtrl', ['$scope', '$routeParams', '$log', 'blogSrvc', function(scope, routeParams, log, blog){
     scope.blog = blog;
     scope.entry;
     
-    if(routeParams.slug && scope.blog.entries){
+    var findEntry = function(){
+        log.debug('Finding matching entry');
         scope.entry = _.find(scope.blog.entries, function(entry){
             return entry.slug == routeParams.slug;
+        });
+    }
+    if(routeParams.slug && scope.blog.entries){
+        findEntry();
+    } else if(!scope.blog.entries){
+        var deregister = scope.$watch(function(){
+            return scope.blog.entries;
+        }, function(){
+            if(scope.blog.entries){
+                log.debug('Entries loaded');
+                findEntry();
+                deregister();
+            }
         });
     }
     
