96b0c3526aa04282a676ad76b42fef4b5154aabe
manufacturedba <manufacturedba@gmail.com>
2014-09-16 12:50:54 -0500

    Added dependencies needed to load comments on page. Added spacing at bottom of entries

--- reboot/static/js/app.js
+++ reboot/static/js/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-var app = angular.module('rebootRoberto', ['ngRoute', 'rebootRoberto.controllers.index', 'rebootRoberto.controllers.entry', 'rebootRoberto.controllers.utility']);
+var app = angular.module('rebootRoberto', ['ngRoute', 'ngDisqus', 'rebootRoberto.controllers.index', 'rebootRoberto.controllers.entry', 'rebootRoberto.controllers.utility']);
 
-app.config(['$routeProvider', '$locationProvider', function(routeProvider, locationProvider){
+app.config(['$routeProvider', '$locationProvider', '$disqusProvider', function(routeProvider, locationProvider, disqusProvider){
     routeProvider.
         when('/', {templateUrl: 'partials/index.html', controller: 'indexCtrl'}).
         when('/entry/:slug', {templateUrl: 'partials/entry.html', controller: 'entryCtrl'}).
         otherwise({redirectTo: '/'});
     locationProvider.html5Mode(true);
+    disqusProvider.setShortname('rebootroberto');
 }]);
 
 app.run(['blogSrvc', function(blog){
